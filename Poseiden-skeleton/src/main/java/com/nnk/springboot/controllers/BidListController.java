76aa952173a6f314bba57db63fbcecb0232746f3
Clément <jour.clement@gmail.com>
2021-09-10 17:04:16 +0200

    CRUD implementation for BidList controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.services.BidListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,16 +12,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
+
+    @Autowired
+    BidListService bidListService;
 
     @RequestMapping("/bidList/list")
     public String home(Model model)
     {
-        // TODO: call service find all bids to show to the view
+        List<BidList> listOfBidList = bidListService.findAll();
+        model.addAttribute("listOfBidList", listOfBidList);
         return "bidList/list";
     }
 
@@ -29,27 +35,47 @@ public class BidListController {
     }
 
     @PostMapping("/bidList/validate")
-    public String validate(@Valid BidList bid, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return bid list
+    public String validate(@Valid BidList bidList, BindingResult result, Model model) {
+        //Recheck interest of BindingResult class.
+        if (!result.hasErrors()) {
+            bidListService.save(bidList);
+            return "redirect:/bidList/list";
+        }
         return "bidList/add";
     }
 
     @GetMapping("/bidList/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Bid by Id and to model then show to the form
+        BidList bidList = bidListService.findById(id);
+        if(bidList != null) {
+            model.addAttribute("bidList", bidList);
+        }
         return "bidList/update";
     }
 
+    //TODO:find out why it can't retrieve correctly the ID
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Bid and return list Bid
+
+        System.out.println(bidList.getBidListId());
+        System.out.println(bidList.getAccount());
+
+
+        if(!result.hasErrors()) {
+            bidListService.save(bidList);
+        } else {
+            return "bidList";
+        }
         return "redirect:/bidList/list";
     }
 
     @GetMapping("/bidList/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Bid by Id and delete the bid, return to Bid list
+        BidList bidList = bidListService.findById(id);
+        if(bidList != null) {
+            bidListService.delete(bidList.getBidListId());
+        }
         return "redirect:/bidList/list";
     }
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.sql.Date;
 import java.sql.Timestamp;
 
 @Entity
@@ -13,8 +12,7 @@ import java.sql.Timestamp;
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "BidListId")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer bidListId;
 
     @NotBlank(message = "Account is mandatory")
@@ -23,73 +21,25 @@ public class BidList {
     @NotBlank(message = "Type is mandatory")
     private String type;
 
-    @Digits(integer = 10, fraction = 2)
+    @Digits(integer = 10, fraction = 0)
     @Min(value = 1)
     @NotNull
     private Double bidQuantity;
 
+    @Digits(integer = 10, fraction = 0)
+    @Min(value = 1)
+    @NotNull
     private Double askQuantity;
-
-    private Double bid;
-
-    private Double ask;
-
-    private String benchmark;
-
-    private Timestamp bidListDate;
-
-    private String commentary;
-
-    private String security;
-
-    private String status;
-
-    private String trader;
-
-    private String book;
-
-    private String creationName;
-
-    private Timestamp creationDate;
-
-    private String revisionName;
-
-    private Timestamp revisionDate;
-
-    private String dealName;
-
-    private String dealType;
-
-    private String sourceListId;
-
-    private String side;
 
     public BidList() {
     }
 
-    public BidList(Integer bidListId, String account, String type, Double bidQuantity, Double askQuantity, Double bid, Double ask, String benchmark, Timestamp bidListDate, String commentary, String security, String status, String trader, String book, String creationName, Timestamp creationDate, String revisionName, Timestamp revisionDate, String dealName, String dealType, String sourceListId, String side) {
+    public BidList(Integer bidListId, String account, String type, Double bidQuantity, Double askQuantity) {
         this.bidListId = bidListId;
         this.account = account;
         this.type = type;
         this.bidQuantity = bidQuantity;
         this.askQuantity = askQuantity;
-        this.bid = bid;
-        this.ask = ask;
-        this.benchmark = benchmark;
-        this.bidListDate = bidListDate;
-        this.commentary = commentary;
-        this.security = security;
-        this.status = status;
-        this.trader = trader;
-        this.book = book;
-        this.creationName = creationName;
-        this.creationDate = creationDate;
-        this.revisionName = revisionName;
-        this.revisionDate = revisionDate;
-        this.dealName = dealName;
-        this.dealType = dealType;
-        this.sourceListId = sourceListId;
-        this.side = side;
     }
 
     public Integer getBidListId() {
@@ -132,139 +82,4 @@ public class BidList {
         this.askQuantity = askQuantity;
     }
 
-    public Double getBid() {
-        return bid;
-    }
-
-    public void setBid(Double bid) {
-        this.bid = bid;
-    }
-
-    public Double getAsk() {
-        return ask;
-    }
-
-    public void setAsk(Double ask) {
-        this.ask = ask;
-    }
-
-    public String getBenchmark() {
-        return benchmark;
-    }
-
-    public void setBenchmark(String benchmark) {
-        this.benchmark = benchmark;
-    }
-
-    public Timestamp getBidListDate() {
-        return bidListDate;
-    }
-
-    public void setBidListDate(Timestamp bidListDate) {
-        this.bidListDate = bidListDate;
-    }
-
-    public String getCommentary() {
-        return commentary;
-    }
-
-    public void setCommentary(String commentary) {
-        this.commentary = commentary;
-    }
-
-    public String getSecurity() {
-        return security;
-    }
-
-    public void setSecurity(String security) {
-        this.security = security;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getTrader() {
-        return trader;
-    }
-
-    public void setTrader(String trader) {
-        this.trader = trader;
-    }
-
-    public String getBook() {
-        return book;
-    }
-
-    public void setBook(String book) {
-        this.book = book;
-    }
-
-    public String getCreationName() {
-        return creationName;
-    }
-
-    public void setCreationName(String creationName) {
-        this.creationName = creationName;
-    }
-
-    public Timestamp getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Timestamp creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getRevisionName() {
-        return revisionName;
-    }
-
-    public void setRevisionName(String revisionName) {
-        this.revisionName = revisionName;
-    }
-
-    public Timestamp getRevisionDate() {
-        return revisionDate;
-    }
-
-    public void setRevisionDate(Timestamp revisionDate) {
-        this.revisionDate = revisionDate;
-    }
-
-    public String getDealName() {
-        return dealName;
-    }
-
-    public void setDealName(String dealName) {
-        this.dealName = dealName;
-    }
-
-    public String getDealType() {
-        return dealType;
-    }
-
-    public void setDealType(String dealType) {
-        this.dealType = dealType;
-    }
-
-    public String getSourceListId() {
-        return sourceListId;
-    }
-
-    public void setSourceListId(String sourceListId) {
-        this.sourceListId = sourceListId;
-    }
-
-    public String getSide() {
-        return side;
-    }
-
-    public void setSide(String side) {
-        this.side = side;
-    }
 }
