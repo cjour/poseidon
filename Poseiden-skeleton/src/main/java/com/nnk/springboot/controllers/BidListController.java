acdd18b275d1b369dd7a3fd11eb343b84f8d3441
Clément <jour.clement@gmail.com>
2021-09-20 15:10:26 +0200

    LOGGER bidList

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -2,6 +2,8 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.services.BidListService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,6 +20,7 @@ import java.util.List;
 @Controller
 public class BidListController {
 
+    private static final Logger LOGGER = LogManager.getLogger(BidListController.class);
     @Autowired
     BidListService bidListService;
 
@@ -28,11 +31,13 @@ public class BidListController {
     {
         List<BidList> listOfBidList = bidListService.findAll();
         model.addAttribute("listOfBidList", listOfBidList);
+        LOGGER.info("BidListController (home) -> " + listOfBidList.size() + "bidList(s) found");
         return "bidList/list";
     }
 
     @GetMapping("/bidList/add")
     public String addBidForm(BidList bid) {
+        LOGGER.info("BidListController (add) -> retrieving add form");
         return "bidList/add";
     }
 
@@ -40,8 +45,10 @@ public class BidListController {
     public String validate(@Valid BidList bidList, BindingResult result, Model model) {
         if (!result.hasErrors()) {
             bidListService.save(bidList);
+            LOGGER.info("BidListController (save) -> bidList successfully added");
             return "redirect:/bidList/list";
         }
+        LOGGER.info("BidListController (save) -> Invalid fields values");
         return "bidList/add";
     }
 
@@ -49,18 +56,23 @@ public class BidListController {
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         BidList bidList = bidListService.findById(id);
         if(bidList != null) {
+            LOGGER.info("BidListController (update) -> bidList successfully found");
             model.addAttribute("bidList", bidList);
+        } else {
+            LOGGER.info("BidListController (save) -> bidList unsuccessfully found");
         }
         return "bidList/update";
     }
 
-    //TODO:find out why it can't retrieve correctly the ID
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
                              BindingResult result, Model model) {
 
         if(!result.hasErrors()) {
             bidListService.save(bidList);
+            LOGGER.info("BidListController (update) -> bidList successfully update");
+        } else {
+            LOGGER.info("BidListController (update) -> bidList unsuccessfully updated");
         }
         return "redirect:/bidList/list";
     }
@@ -70,6 +82,9 @@ public class BidListController {
         BidList bidList = bidListService.findById(id);
         if(bidList != null) {
             bidListService.delete(bidList.getId());
+            LOGGER.info("BidListController (delete) -> bidList successfully deleted");
+        } else {
+            LOGGER.info("BidListController (delete) -> bidList unsuccessfully deleted");
         }
         return "redirect:/bidList/list";
     }
